7c53380e23fa5d9519f9026e495c4bf3632008e9
Caesar0910 <cooker98910@gmail.com>
2022-03-11 19:07:28 +0100

    re-build

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,107 +1,169 @@
+import React, {useState} from 'react';
 import './dashboard.css';
+import {HiPlusSm, HiMinusSm} from 'react-icons/hi'
+import Modal from "react-bootstrap/Modal";
+import { Form, Col, Button, InputGroup } from "react-bootstrap";
+import icon from '../assets/usdc-coin.png';
 
 const Dashboard = () => {
+
+  const [isDepositOpen, setIsDepositOpen] = useState(false);
+  const [isWithdrawOpen, setIsWithdrawOpen] = useState(false);
+  const [data, setData] = useState({
+    to: "",
+    from: ""
+  })
+
+  const showDepositModal = () => {
+    setIsDepositOpen(true);
+    document.body.style.backgroundColor = "white";
+  };
+  const showWithdrawModal = () => { setIsWithdrawOpen(true); }
+  const hideDepositModal = () => { setIsDepositOpen(false); };
+  const hideWithdrawModal = () => { setIsWithdrawOpen(false) }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if(name === 'to' && data.from){
+      setData((prevState) => ({
+        ...prevState,
+        [name]: value,
+        from: ''
+      }));
+    } else {
+      setData((prevState) => ({
+        ...prevState,
+        [name]: value,
+      }));
+    }
+  };
+
+
   return (
-    <div className='d-flex flex-column-fluid'>
-      <div className='container-fluid'>
-        <div className='row balance'>
-          <div className='col-lg-6 col-xxl-12'>
-            <p>Return</p>
-            <p>31.68%</p>
-          </div>
-          <div className='col-lg-6 col-xxl-12'>
-            <p>Your total Balance:</p>
-            <p>$2,212.14</p>
-            <p style={{fontSize: '14px'}}>Withdraw</p>
-          </div>
-        </div>  
-        <div className='row'>
-          <dv className='card card-custom card-stretch gutter-b'>
-            <div className='card-header border-0 py-5'>
-                <h3 className='card-title aligh-item-start flex-column'>
-                  <span className='card-label font-weight-bolder'>Assets you own</span>
-                </h3>
+    <>
+      <div className='d-flex flex-column-fluid'>
+        <div className="container-fluid">
+          <div className="row balance">
+            <div className="col-lg-6 col-md-6">
+              <p>Return</p>
+              <p>31.68%</p>
             </div>
-            <div className='card-body pt-0 pb-3'>
-              <div className='tab-content'>
-                <div className='table-responsive'>
-                  <table className='table table-head-custom table-head-bg table-vertical-center'>
-                    <thead>
-                      <tr>
-                        <th>Aseet/currency</th>
-                        <th>Price</th>
-                        <th>APY</th>
-                        <th>Balance</th>
-                        <th>Total Interest earned</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td className='usdc-buy' colSpan={5}>+ USDC  + BUY</td>
-                      </tr>
-                      <tr>
-                        <td><span style={{backgroundImage: "url('https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png')"}}></span><span className="text-dark-75 font-weight-bolder d-block font-size-lg">stablecoin</span></td>
-                        <td><span className="text-dark-75 font-weight-bolder d-block font-size-lg">$1</span></td>
-                        <td><span className="text-dark-75 font-weight-bolder d-block font-size-lg">6%</span></td>
-                        <td>
-                          <span className="text-dark-75 font-weight-bolder d-block font-size-lg">0.04484529 USDC</span>
-                          <span className="text-muted font-weight-bold">$1,989.90</span>
-                        </td>
-                        <td>
-                          <span className="text-dark-75 font-weight-bolder d-block font-size-lg">0.04484529 USDC</span>
-                          <span className="text-muted font-weight-bold">$1,989.90</span>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>  
-                </div>
+            <div className="col-lg-6 col-md-6">
+              <p>Balance</p>
+              <p>$2,212.14</p>
+              <div className="dropdown"><HiPlusSm /><span className='button' onClick={showDepositModal}>Deposit </span></div>
+              <div className="dropdown"><HiMinusSm /><span className='button' onClick={showWithdrawModal}>Withdraw</span></div>
+            </div>
+          </div>
+          <div className="row assets col-xl-12">
+            <p>Assets you own</p>
+            <div className="table-container" role="table" aria-label="Destinations">
+              <div className="flex-table header" role="rowgroup">
+                <div className="flex-row first" role="columnheader">Asset/Currency</div>
+                <div className="flex-row" role="columnheader">Price</div>
+                <div className="flex-row" role="columnheader">APY</div>
+                <div className="flex-row" role="columnheader">Balance</div>
+                <div className="flex-row" role="columnheader">Total Interest earned</div>
+              </div>
+              <div className="flex-table row" role="rowgroup">
+                <div className="flex-row first" role="cell">
+                  <img src={icon} alt="usdc icon" /> stablecoin</div>
+                <div className="flex-row" role="cell">$1</div>
+                <div className="flex-row" role="cell">6%</div>
+                <div className="flex-row" role="cell">0.04484529 USDC</div>
+                <div className="flex-row" role="cell">0.00066222 USDC</div>
               </div>
             </div>
-          </dv>
-        </div>
-        <div className='row'>
-          <dv className='card card-custom card-stretch gutter-b'>
-            <div className='card-header border-0 py-5'>
-                <h3 className='card-title aligh-item-start flex-column'>
-                  <span className='card-label font-weight-bolder'>Risk strategies</span>
-                </h3>
-            </div>
-            <div className='card-body pt-0 pb-3'>
-              <div className='tab-content'>
-                <div className='table-responsive'>
-                  <table className='table table-head-custom table-head-bg table-vertical-center'>
-                    <thead>
-                      <tr>
-                        <th>Aseet/currency</th>
-                        <th>Price</th>
-                        <th>APY</th>
-                        <th>Balance</th>
-                        <th>Total Interest earned</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td><span className="text-dark-75 font-weight-bolder d-block font-size-lg">stablecoin</span></td>
-                        <td><span className="text-dark-75 font-weight-bolder d-block font-size-lg">$1</span></td>
-                        <td><span className="text-dark-75 font-weight-bolder d-block font-size-lg">8~12%</span></td>
-                        <td>
-                          <span className="text-dark-75 font-weight-bolder d-block font-size-lg">0.04484529 USDC</span>
-                          <span className="text-muted font-weight-bold">$1,989.90</span>
-                        </td>
-                        <td>
-                          <span className="text-dark-75 font-weight-bolder d-block font-size-lg">0.00066222 USDC</span>
-                          <span className="text-muted font-weight-bold">$29.44</span>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>  
-                </div>
+          </div>
+          <div className="row assets col-xl-12">
+            <p>Risk strategies</p>
+            <div className="table-container" role="table" aria-label="Destinations">
+              <div className="flex-table header" role="rowgroup">
+                <div className="flex-row-1 first" role="columnheader">Asset/Currency</div>
+                <div className="flex-row-1" role="columnheader">APY</div>
+                <div className="flex-row-1" role="columnheader">Balance</div>
+              </div>
+              <div className="flex-table row" role="rowgroup">
+                <div className="flex-row-1 first" role="cell">
+                  <img src={icon} alt="usdc icon" /> Stable Strategy</div>
+                <div className="flex-row-1" role="cell">8~12%</div>
+                <div className="flex-row-1" role="cell">0.04484529 USDC</div>
               </div>
             </div>
-          </dv>
+          </div>
         </div>
       </div>
-    </div>
+      <Modal
+        show={isDepositOpen}
+        onHide={hideDepositModal}
+      >
+        <Modal.Header>
+          <Modal.Title>Deposit</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form className='deposit'>
+            <Form.Group>
+              <Form.Label>Select Cryptocurrency</Form.Label>
+              <Form.Control
+                  required
+                  name="to"
+                  as="select"
+                  placeholder="to"
+                  value={data.to} // add this prop
+                  onChange={handleChange}
+                >
+                  <option hidden value="">
+                    Select...
+                  </option>
+                  <option value="crypto">Deposit from Crypto address</option>
+                  <option value="ramp">Deposit from Ramp Network</option>
+              </Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Wallet Address</Form.Label>
+              <Form.Control type="text" placeholder="ROC wallet address" />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button bsstyle='danger' onClick={hideDepositModal}>Cancel</Button>
+          <Button>Deposit</Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal
+        show={isWithdrawOpen}
+        onHide={hideWithdrawModal}
+      >
+        <Modal.Header>
+          <Modal.Title>Withdraw</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form className='deposit'>
+            <Form.Group>
+              <Form.Label>Current Cryptocurrency</Form.Label><br></br>
+              <Form.Label><img src={icon} alt='icon'/> USDC</Form.Label>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Current Balance</Form.Label><br></br>
+              <Form.Label style={{fontSize: "25px"}}>3948 USDC</Form.Label>
+            </Form.Group>
+            <InputGroup>
+              <Form.Control aria-label="Dollar amount (with dot and two decimal places)" />
+              <InputGroup.Text>USDC</InputGroup.Text>
+              <Button variant="outline-secondary">MAX</Button>
+            </InputGroup>
+            <Form.Group>
+              <Form.Label>Wallet Address</Form.Label>
+              <Form.Control type="text" placeholder="ROC wallet address" />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button bsstyle='danger' onClick={hideWithdrawModal}>Cancel</Button>
+          <Button>Withdraw</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   )
 }
 
